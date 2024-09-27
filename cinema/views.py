2bec4aabcb1f60196d4d2a7266710664cb1540a0
Danyil <dartomov@gmail.com>
2024-09-27 17:15:39 +0200

    Fix annotations

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework.views import APIView
 from rest_framework import status, generics, mixins, viewsets
 
@@ -13,12 +14,12 @@ from cinema.serializers import (
 
 
 class GenreList(APIView):
-    def get(self, request):
+    def get(self, request: Request) -> Response:
         genres = Genre.objects.all()
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
+    def post(self, request: Request) -> Response:
         serializer = GenreSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -26,25 +27,25 @@ class GenreList(APIView):
 
 
 class GenreDetail(APIView):
-    def get_object(self, pk):
+    def get_object(self, pk) -> Genre | Response:
         return get_object_or_404(Genre, pk=pk)
 
-    def get(self, request, pk):
+    def get(self, request: Request, pk: int) -> Response:
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def put(self, request, pk, partial=False):
+    def put(self, request: Request, pk: int, partial=False) -> Response:
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
-    def patch(self, request, pk):
+    def patch(self, request: Request, pk: int) -> Response:
         return self.put(request, pk, partial=True)
 
-    def delete(self, request, pk):
+    def delete(self, request: Request, pk: int) -> Response:
         genre = self.get_object(pk)
         genre.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -58,10 +59,10 @@ class ActorList(
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request: Request, *args, **kwargs) -> Response:
         return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request: Request, *args, **kwargs) -> Response:
         return self.create(request, *args, **kwargs)
 
 
@@ -74,16 +75,16 @@ class ActorDetail(
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request: Request, *args, **kwargs) -> Response:
         return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
+    def put(self, request: Request, *args, **kwargs) -> Response:
         return self.update(request, *args, **kwargs)
 
-    def patch(self, request, *args, **kwargs):
+    def patch(self, request: Request, *args, **kwargs) -> Response:
         return self.partial_update(request, *args, **kwargs)
 
-    def delete(self, request, *args, **kwargs):
+    def delete(self, request: Request, *args, **kwargs) -> Response:
         return self.destroy(request, *args, **kwargs)
 
 
